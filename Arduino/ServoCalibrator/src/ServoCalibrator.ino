7775ddc1b3b128c5b5c7511a5f5fef49150de17f
Thar0l <gusarov.aa@gmail.com>
2016-10-16 16:35:56 +0300

    Added servo calibrator arduino firmware.

---
+++ Arduino/ServoCalibrator/src/ServoCalibrator.ino
@@ -0,0 +1,25 @@
+#include <Servo.h>
+/**************************/
+const int SERVO_PIN = 2;
+
+/**************************/
+Servo myservo;
+/*************************/
+
+
+void setup() {
+    Serial.begin(57600);
+    myservo.attach(SERVO_PIN);
+    myservo.writeMicroseconds(1000);
+}
+
+void loop() {
+    while(Serial.available() > 0)
+    {
+        int pos = Serial.parseInt();
+        if (pos >= 500)
+        {
+            myservo.writeMicroseconds(pos);
+        }
+    }
+}
